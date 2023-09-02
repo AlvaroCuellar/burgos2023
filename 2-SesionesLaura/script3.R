3a6e512346b0e592fc524f6adca4f3907e15e620
Laura HL <40395141+lamusadecima@users.noreply.github.com>
2023-09-02 18:38:54 +0200

    Add files via upload

---
+++ 2-SesionesLaura/script3.R
@@ -0,0 +1,26 @@
+#################################################
+##                                             ##
+##        FILOLOGÍA DIGITAL E                  ##
+##        INTELIGENCIA ARTIFICIAL              ##
+##                                             ##
+##        Talleres de Estilometría             ##
+##                                             ##
+##            Script 3                         ##
+##                                             ##
+#################################################
+
+# Primero establecemos el directorio de trabajo
+# Si tienes un Mac y has guardado la carpeta "Roman-de-la-Rose" en el escritorio:
+setwd("Desktop/RdlR_for_rolling_classify")
+# Si tienes Windows y has guardado la carpeta en C:
+setwd("C:/RdlR_for_rolling_classify")
+
+# A continuación, llamamos al paquete stylo
+
+library(stylo)
+
+# Para Rolling Classify
+
+rolling.classify(write.png.file = TRUE, classification.method = "svm", mfw=100, training.set.sampling = "normal.sampling", slice.size = 5000, slice.overlap = 4500) 
+rolling.classify(write.png.file = TRUE, classification.method = "nsc", mfw=50, training.set.sampling = "normal.sampling", slice.size = 5000, slice.overlap = 4500)
+rolling.classify(write.png.file = TRUE, classification.method = "delta", mfw=500)
